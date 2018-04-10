c8f800df3733e88cc29aa5245b320fc9ef84e794
wkimak <wkimak94@gmail.com>
2018-04-10 17:11:22 -0400

    Built NavBar component, added basic styles

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,7 +5,13 @@ import React from 'react';
 /* ----------- Level 2 ------------- */
 
 const NavBar = (props) => {
-	return <div></div>;
+	return (
+      <div className='navbar_container'>
+        <p>Devterest</p>
+        <button>Login</button>
+        <button>Add</button>
+      </div>
+	); 
 };
 
 export default NavBar;
