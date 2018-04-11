f4a8bf51ef820d4d095020fdd1971bd596dc8c9b
wkimak <wkimak94@gmail.com>
2018-04-11 16:35:00 -0400

    Added tags to each link

--- controllers/linkControllers.js
+++ controllers/linkControllers.js
@@ -2,11 +2,32 @@ const { knex } = require('../database/index');
 
 exports.getAllLinks = async (req, res) => {
 	const allLinks = await knex.select().from('links').orderBy('votes', 'desc');
+
+	// 1.) Loop through allLinks array, select link ID
+	// 2.) Look into LinksTags table
+	       // Get all tag Ids that match that link ID
+	// 3.) Go to Tags table, select tagName of tagIDs
+	// 4.) res.send(links)
+
+	for(let i = 0; i < allLinks.length; i++){
+		const tagsId = await knex('linksTags').select('tags_id').where({links_id: allLinks[i].id_links});
+		
+		for(let x = 0; x < tagsId.length; x++){
+          let tag = await knex('tags').select('tagName').where({id_tags: tagsId[x].tags_id});
+          if(allLinks[i].tagName){
+            allLinks[i].tagName.push(tag[0].tagName); 
+          } else {
+          	allLinks[i].tagName = [tag[0].tagName];
+          }
+		}
+	}
+
 	res.send(allLinks);
 };
 
 exports.addLink = async (req, res) => {
 	const { url, kind, votes, username, tagName } = req.body;
+
 	const isLink = await knex.select('url').from('links').where({ url: url });
 	const userId = await knex.select('id_users').from('users').where({ username: username });
 
@@ -25,7 +46,7 @@ exports.addLink = async (req, res) => {
 
 	for (let i = 0; i < tagName.length; i++) {
 		const tagId = await knex.select('id_tags').from('tags').where({ tagName: tagName[i] });
-		const isLinkTagged = await knex('LinksTags').select('links_id').where({ tags_id: tagId[0].id_tags });
+		const isLinkTagged = await knex('LinksTags').select('tags_id').where({ links_id: url });
 
 		if (!isLinkTagged.length) {
 			const addingToTagsJoin = await knex('LinksTags').insert({ links_id: linkId[0].id_links, tags_id: tagId[0].id_tags });
