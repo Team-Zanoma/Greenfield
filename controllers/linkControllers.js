739e87bb37c0d609b866d8c2721e68f1c8f55533
wkimak <wkimak94@gmail.com>
2018-04-11 17:23:23 -0400

    Implemented metaData, added title, description, image to links table

--- controllers/linkControllers.js
+++ controllers/linkControllers.js
@@ -1,20 +1,15 @@
 const { knex } = require('../database/index');
+const urlMetadata = require('url-metadata');
 
 exports.getAllLinks = async (req, res) => {
 	const allLinks = await knex.select().from('links').orderBy('votes', 'desc');
-
-	// 1.) Loop through allLinks array, select link ID
-	// 2.) Look into LinksTags table
-	       // Get all tag Ids that match that link ID
-	// 3.) Go to Tags table, select tagName of tagIDs
-	// 4.) res.send(links)
 
 	for(let i = 0; i < allLinks.length; i++){
 		const tagsId = await knex('linksTags').select('tags_id').where({links_id: allLinks[i].id_links});
 		
 		for(let x = 0; x < tagsId.length; x++){
           let tag = await knex('tags').select('tagName').where({id_tags: tagsId[x].tags_id});
-          if(allLinks[i].tagName){
+          if(allLinks[i].tagName && !allLinks[i].tagName.includes(tag[0].tagName)) {
             allLinks[i].tagName.push(tag[0].tagName); 
           } else {
           	allLinks[i].tagName = [tag[0].tagName];
@@ -28,15 +23,17 @@ exports.getAllLinks = async (req, res) => {
 exports.addLink = async (req, res) => {
 	const { url, kind, votes, username, tagName } = req.body;
 
+    // Inserting meta data to links table
+    const metaData = await urlMetadata(url);
+   
 	const isLink = await knex.select('url').from('links').where({ url: url });
 	const userId = await knex.select('id_users').from('users').where({ username: username });
 
 	if (isLink.length === 0) {
-		const addingLink = await knex('links').insert({ url: url, kind: kind });
+		const addingLink = await knex('links').insert({ url: url, kind: kind, title: metaData['og:title'], description: metaData['og:description'], image: metaData['og:image'] });
 		var linkId = await knex.select('id_links').from('links').where({ url: url });
 		const addingToLinkJoin = await knex('UsersLinks').insert({ links_id: linkId[0].id_links, users_id: userId[0].id_users });
-	}
-	else {
+	} else {
 		const linkCount = await knex('links').where({ url: url }).select('shares');
 		const updatedCount = linkCount[0].shares + 1;
 		const upShareCount = await knex('links').where({ url: url }).update({ shares: updatedCount });
