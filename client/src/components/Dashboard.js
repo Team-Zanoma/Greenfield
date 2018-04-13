8e70e3dd3663847536d06a75d36a6a38e31c4652
mrMustachos <bill.domanick@crowdsurge.com>
2018-04-13 13:54:37 -0400

    cleaning up tag suggestions

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -8,38 +8,40 @@ import styles3 from '../assets/sass/Files.module.scss';
 const Dashboard = function({hideDashboard, favoritesList}) {
 	return(
       <div className={ styles.dashboard_overlay }>
-        <button className={`${ styles.btn } ${ styles.close2 }`}>
-          <i className={ styles.btn__icon } onClick={() => hideDashboard() }>close</i>
-        </button>
-        <h2> Favorites </h2>
-        <div className={ styles2.feed_container }>
-          <ul>
-          {favoritesList.map((file) => {
-          	return(
-              <li className={ styles3.file_container }>
-               <img src={ file[0].image } alt="" />
-  	            <div className={ styles3.details }>
-  	              <h3>
-                   <a href={ file[0].url } target='_blank'>{ file[0].title }</a>
-                 </h3>
-  	              <div>{ file[0].description }</div>
-  	              <div className={ styles3.metrics }>
-                   <ul>
-                     <li>
-                       <span className="title"><i className={ styles3.share__icon }>star_border</i>Liked</span>
-                       <span className="count">{ file[0].likes }</span>
-                     </li>
-                     <li>
-                       <span className="title"><i className={ styles3.share__icon }>share</i>Shares</span>
-                       <span className="count">{ file[0].shares }</span>
-                     </li>
-                   </ul>
-                 </div>
-  	            </div>
-              </li>
-          	);
-          })}
-          </ul>
+        <div className={ styles.addSource_container }>
+          <button className={`${ styles.btn } ${ styles.close2 }`}>
+            <i className={ styles.btn__icon } onClick={() => hideDashboard() }>close</i>
+          </button>
+          <h2> Favorites </h2>
+          <div className={ styles2.feed_container }>
+            <ul>
+            {favoritesList.map((file) => {
+              return(
+                <li className={ styles3.file_container }>
+                 <img src={ file[0].image } alt="" />
+                  <div className={ styles3.details }>
+                    <h3>
+                     <a href={ file[0].url } target='_blank'>{ file[0].title }</a>
+                   </h3>
+                    <div>{ file[0].description }</div>
+                    <div className={ styles3.metrics }>
+                     <ul>
+                       <li>
+                         <span className="title"><i className={ styles3.share__icon }>star_border</i>Liked</span>
+                         <span className="count">{ file[0].likes }</span>
+                       </li>
+                       <li>
+                         <span className="title"><i className={ styles3.share__icon }>share</i>Shares</span>
+                         <span className="count">{ file[0].shares }</span>
+                       </li>
+                     </ul>
+                   </div>
+                  </div>
+                </li>
+              );
+            })}
+            </ul>
+          </div>
         </div>
       </div>
 	);
